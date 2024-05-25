fce6a1e7a599e71bb7c4fe54ff273b8290139c81
Victory <onorhimevictory2017@gmail.com>
2024-05-25 10:09:55 -0400

    new commit

--- public/index.php
+++ public/index.php
@@ -1,9 +1,22 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+
 return function (array $context) {
+    $request = Request::createFromGlobals();
+    $response = new Response();
+
+    // Serve the index.html file directly
+    if ($request->getPathInfo() === '/') {
+        $response->setContent(file_get_contents(__DIR__.'/index.html'));
+        $response->send();
+        exit;
+    }
+
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
